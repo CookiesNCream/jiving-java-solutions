4d48aab36b68b16e031308ec6611c53c0311c15b
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-04-06 17:33:30 +0800

    Create more-review-more-linked-lists.java

---
+++ more-review-more-linked-lists.java
@@ -0,0 +1,15 @@
+public static Node removeDuplicates(Node head) {
+         if (head == null) {
+            return head;
+         } else {
+               Node current = head;
+               while (current.next != null) {
+                    if (current.data == current.next.data) {
+                       current.next = current.next.next;        
+                    } else {
+                        current = current.next;
+                    }
+               }
+        return head;
+        }
+}
