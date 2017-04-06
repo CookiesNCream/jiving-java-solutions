e5fab0c8df52db87af6ae60486e9c2bedf107341
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-04-06 18:13:14 +0800

    Create data-types.java

---
+++ data-types.java
@@ -0,0 +1,18 @@
+public class DataTypesPractice {
+        
+    
+    public static void main(String[] args) {
+      // Write your print statements here! You should have at least 8 of them.
+      System.out.println("Primitive : Double");
+      System.out.println("Primitive : Character");
+      System.out.println("Primitive : Boolean");
+      System.out.println("Primitive : Integer");
+      System.out.println("Referenced : String");
+      System.out.println("Primitive : Boolean");
+      System.out.println("Primitive : Double");
+      System.out.println("Primitive : Character");
+      System.out.println("Referenced : String");
+           
+      
+    }
+}
