ce6080b2a5dcd7568075bdf31acb09cc51f1cc04
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-04-06 17:37:50 +0800

    Create recursion.java

---
+++ recursion.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int gcd=find_gcd(a,b);
+        System.out.println(gcd);
+    }
+    static  int find_gcd(int a,int b) {
+          if (a == 0 || b == 0) {
+              return a + b;
+          } else {
+              return find_gcd(b,a%b);
+          }
+          
+    }
+}
