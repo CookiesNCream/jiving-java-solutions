31880ad6bc141ab238cd08071be6ae5133539303
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-04-06 18:08:04 +0800

    Create binary-numbers.java

---
+++ binary-numbers.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            System.out.println(Integer.toBinaryString(n));
+         }  
+    }
+}
