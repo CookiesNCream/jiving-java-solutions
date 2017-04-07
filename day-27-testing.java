e08be854a2e52d12b586d96b23426fbdc7b49839
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-04-07 17:22:18 +0800

    Create day-27-testing.java

---
+++ day-27-testing.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //Number of Test Cases
+        System.out.println("5");
+        // Test Case 1 : YES
+        System.out.println("4 3");
+        System.out.println("-2 0 1 3");
+        // Test Case 2 : NO
+        System.out.println("3 2");
+        System.out.println("-1 0 4");
+        // Test Case 3 : YES
+        System.out.println("5 3");
+        System.out.println("-3 0 2 1 4");
+        // Test Case 4 : NO
+        System.out.println("6 3");
+        System.out.println("-2 0 -1 2 3 1");
+        // Test Case 5 : YES
+        System.out.println("7 4");
+        System.out.println("-1 0 -3 2 1 4 3");
+    }
+}
