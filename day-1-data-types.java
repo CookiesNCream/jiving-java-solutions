3fec868818796bdae3ebf1e8b6a71e7545502a4c
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-04-07 13:35:12 +0800

    Create day-1-data-types.java

---
+++ day-1-data-types.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+        int i1 = 0;
+        double d1 = 0.0;
+        String s1 = "";
+        /* Read and save an integer, double, and String to your variables.*/
+        i1 = scan.nextInt();
+        d1 = scan.nextDouble();
+        scan.nextLine();
+        s1 = scan.nextLine();
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+i1);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d+d1);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s+s1);
+        scan.close();
+    }
+}
